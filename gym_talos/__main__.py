c961b2701cd1360424604de86c8fe9d8a850aebd
Côme Perrot <cperrot@laas.fr>
2022-10-31 17:31:43 +0100

    Now saving the model at the end of training

--- gym_talos/__main__.py
+++ gym_talos/__main__.py
@@ -10,6 +10,9 @@ from gym_talos.envs.env_talos_base import EnvTalosBase
 ################
 display = True
 
+tensorboard_log_dir = "./logs/"
+tensorboard_log_name = "test"
+
 # Parameters filename
 filename = "./gym_talos/config_RL.yaml"
 
@@ -17,6 +20,7 @@ with open(filename, "r") as paramFile:
     params = yaml.safe_load(paramFile)
 params_designer = params["designer"]
 params_env = params["env"]
+params_training = params["training"]
 
 ##############
 #  TRAINING  #
@@ -27,9 +31,14 @@ envTrain = EnvTalosBase(params_designer, params_env, GUI=False)
 # # Automatically normalize the input features and reward
 # env = VecNormalize(env, norm_obs=True, norm_reward=False,
 #                    clip_obs=10.)
-model = PPO("MlpPolicy", envTrain, verbose=1, tensorboard_log="./logs")
-model.learn(total_timesteps=100000)
+model = PPO("MlpPolicy", envTrain, verbose=1, tensorboard_log=tensorboard_log_dir)
+model.learn(
+    total_timesteps=params_training["totalTimesteps"], tb_log_name=tensorboard_log_name
+)
+
 envTrain.close()
+
+model.save(tensorboard_log_dir + tensorboard_log_name)
 
 if display:
     envDisplay = EnvTalosBase(params_designer, params_env, GUI=True)
